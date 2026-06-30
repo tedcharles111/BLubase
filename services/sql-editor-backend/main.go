5177261cee13fc5a517105e315906c01a5d99cf6
Ted Charles <frikeallarry@gmail.com>
2026-06-30 15:47:28 +0000

    Add fmt import and UUID-to-string helper to SQL editor

--- services/sql-editor-backend/main.go
+++ services/sql-editor-backend/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -12,8 +13,8 @@ import (
 	"time"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 var pool *pgxpool.Pool
@@ -24,7 +25,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	// CRITICAL: disable prepared statement caching – prevents "prepared statement already in use"
+	// Disable prepared statement caching – prevents "prepared statement already in use"
 	config.ConnConfig.DefaultQueryExecMode = pgx.QueryExecModeSimpleProtocol
 
 	pool, err = pgxpool.NewWithConfig(context.Background(), config)
@@ -41,23 +42,23 @@ func main() {
 
 	r := chi.NewRouter()
 	r.Get("/sql", runSQLHandler)
-	r.Post("/sql", runSQLHandler) // <-- allow POST as well
+	r.Post("/sql", runSQLHandler)
 	r.Get("/history", historyHandler)
 	r.Post("/import", importHandler)
 
-	log.Println("SQL Editor on :3007 – prepared statements DISABLED")
+	log.Println("SQL Editor on :3007 – prepared statements DISABLED, UUIDs returned as strings")
 	log.Fatal(http.ListenAndServe(":3007", r))
 }
 
-func runSQLHandler(w http.ResponseWriter, r *http.Request) {
-	// Helper to convert byte arrays to UUID strings
-	toUUID := func(val interface{}) interface{} {
-		if b, ok := val.([]byte); ok && len(b) == 16 {
-			return fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
-		}
-		return val
+// Helper to convert byte arrays to UUID strings
+func toUUID(val interface{}) interface{} {
+	if b, ok := val.([]byte); ok && len(b) == 16 {
+		return fmt.Sprintf("%08x-%04x-%04x-%04x-%012x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
 	}
+	return val
+}
 
+func runSQLHandler(w http.ResponseWriter, r *http.Request) {
 	var query string
 	if r.Method == "POST" {
 		var req struct{ Query string `json:"query"` }
