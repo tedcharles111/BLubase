a8e98ccc2df529866c9ec25d6c5ab5e34019dd1b
Ted Charles <frikeallarry@gmail.com>
2026-07-12 18:04:04 +0000

    Add /invoke endpoint to edge functions

--- services/edge-functions/main.go
+++ services/edge-functions/main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
-	"log"
-	"net/http"
+    "io"
+    "log"
+    "net/http"
+    "os/exec"
+    "strings"
 
-	"github.com/go-chi/chi/v5"
+    "github.com/go-chi/chi/v5"
 )
 
 func main() {
-	r := chi.NewRouter()
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("edge functions ready"))
-	})
-	log.Println("Edge Functions on :3005")
-	log.Fatal(http.ListenAndServe(":3005", r))
+    r := chi.NewRouter()
+
+    r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte("edge functions ready"))
+    })
+
+    r.Post("/invoke/{name}", func(w http.ResponseWriter, r *http.Request) {
+        name := chi.URLParam(r, "name")
+        body, _ := io.ReadAll(r.Body)
+        // Execute Deno (must be installed in the container)
+        cmd := exec.Command("deno", "eval", string(body))
+        output, err := cmd.CombinedOutput()
+        if err != nil {
+            w.WriteHeader(500)
+            w.Write([]byte(err.Error()))
+            return
+        }
+        w.Write(output)
+    })
+
+    log.Println("Edge Functions on :3005")
+    log.Fatal(http.ListenAndServe(":3005", r))
 }
